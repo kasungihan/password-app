1175fbc34c0c9275043f83c1f6bab77ea5454667
Kasun Gihan <kasungihan.dev@gmail.com>
2024-02-25 13:05:06 +0000

    ✨ feat: list account details

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,43 @@
-class Table {
+class Account {
+  data = [
+    {
+      title: "Gmail",
+      url: "https://mail.google.com/",
+      username: "app@gmail.com",
+      password: "123456",
+    },
+    {
+      title: "Version",
+      url: "https://gitlab.com/",
+      username: "app@gmail.com",
+      password: "1234",
+    },
+  ];
   constructor(table) {
     this.table = table;
+
     //this.table.addEventListener("click", this.handleClick.bind(this));
   }
 
+  addItem() {}
+
+  removeItem() {}
+
+  displyItem() {
+    let template = "";
+    this.data.forEach((element) => {
+      template += `<div>
+      <div>Title: <b>${element.title}</b></div>
+      <div>URL: <b>${element.url}</b></div>
+      <div>Username: <b>${element.username}</b></div>
+      <div>Password: <b>${element.password}</b></div>
+      </div></br>`;
+    });
+    this.table.innerHTML += template;
+  }
+
   getItem() {
-    fetch("./data.josn")
+    /* fetch("./data.json")
       .then((response) => {
         return response.json();
       })
@@ -14,10 +46,12 @@ class Table {
       })
       .catch((error) => {
         console.error(error);
-      });
+      }); */
   }
 }
 
-const ItemTable = document.getElementById("table");
-const dataTable = new Table(ItemTable);
-dataTable.getItem();
+const ItemTable = document.getElementById("account");
+const dataTable = new Account(ItemTable);
+dataTable.displyItem();
+
+//const addForm = document.getElementById("formsubmit");
